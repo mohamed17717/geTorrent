c8749c2cf25e0b051935babcd99e29020c1ec6c2
mohamed17717 <root@localhost.localdomain>
2019-07-13 06:43:20 +0200

    subtitle affecte by change in vuex film info

--- src/components/Subtitle.vue
+++ src/components/Subtitle.vue
@@ -5,5 +5,26 @@
 </template>
 
 <script>
-export default {};
+import { mapGetters } from "vuex";
+
+export default {
+  name: "Subtitle",
+  computed: {
+    name() {
+      if (this.getFilmInfo.name) {
+        return this.getFilmInfo.name;
+      }
+      return "";
+    },
+
+    year() {
+      if (this.getFilmInfo.year) {
+        return this.getFilmInfo.year;
+      }
+      return "";
+    },
+
+    ...mapGetters(["getFilmInfo"])
+  }
+};
 </script>
