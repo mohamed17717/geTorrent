07390d606462c4a6641d25d4dfcb8ca6fb657c6d
mohamed17717 <root@localhost.localdomain>
2019-07-13 12:24:36 +0200

    create Images componet to contain images about the movie

---
+++ src/components/Images.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="images" v-show="getFilmPictures.length">
+    <h3>صور من الفيلم</h3>
+    <div class="image" v-for="(pic, index) in getFilmPictures" :key="index">
+      <img :src="pic" />
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from "vuex";
+
+export default {
+  name: "images",
+  computed: {
+    ...mapGetters(["getFilmPictures"])
+  }
+};
+</script>
