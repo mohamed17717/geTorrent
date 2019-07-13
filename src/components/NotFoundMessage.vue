5389af649b067a67fa7f9d8c9f0e846e35899779
mohamed17717 <root@localhost.localdomain>
2019-07-13 17:38:15 +0200

    create not found msg component

---
+++ src/components/NotFoundMessage.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="msg" v-if="getProgressBar >= 100 && !getFilmCover">
+    ملاقيتش نتايج بحث مطابقة ... اتأكد من الاسم و السنة
+  </div>
+</template>
+
+<script>
+import { mapGetters } from "vuex";
+
+export default {
+  name: "not-found-message",
+  computed: {
+    ...mapGetters(["getFilmCover", "getProgressBar"])
+  }
+};
+</script>
