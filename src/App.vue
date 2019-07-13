41a9289ba18d3cb2cb6618cef39b78e0bf737712
mohamed17717 <root@localhost.localdomain>
2019-07-13 11:52:50 +0200

    add meta info in app.vue

--- src/App.vue
+++ src/App.vue
@@ -20,6 +20,32 @@ export default {
     // Download,
     // Images,
     Bottom
+  },
+
+  metaInfo: {
+    title: "geTorrent",
+    link: [
+      // logo font
+      {
+        rel: "stylesheet",
+        href:
+          "https://fonts.googleapis.com/css?family=Electrolize&amp;display=swap"
+      },
+      // arabic font
+      {
+        rel: "stylesheet",
+        href: "https://fonts.googleapis.com/css?family=Tajawal&amp;display=swap"
+      },
+      // icons fontawesome
+      {
+        rel: "stylesheet",
+        href:
+          "https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css",
+        integrity:
+          "sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN",
+        crossorigin: "anonymous"
+      }
+    ]
   }
 };
 </script>
