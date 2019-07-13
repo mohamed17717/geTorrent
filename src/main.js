23ffb9dee542abd0927acdb3d68a4f7cd4a695fa
mohamed17717 <root@localhost.localdomain>
2019-07-13 09:05:59 +0200

    start use myplugins

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import Vue from "vue";
 import App from "@/App.vue";
 import store from "@/store";
+import MyPlugin from "@/plugins";
 
 Vue.config.productionTip = false;
+
+Vue.use(MyPlugin);
 
 new Vue({
   store,
