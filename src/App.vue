0067a96d058c9e6ff712192ca985f1c6716919fd
mohamed17717 <root@localhost.localdomain>
2019-07-13 06:17:48 +0200

    add search component

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <div id="app">
+    <Search />
     <Torrent />
     <Subtitle />
   </div>
 </template>
 
 <script>
+import Search from "./components/Search.vue";
 import Torrent from "./components/Torrent.vue";
 import Subtitle from "./components/Subtitle.vue";
 
 export default {
   name: "app",
   components: {
+    Search,
     Torrent,
     Subtitle
   }
---
+++ src/components/Search.vue
@@ -0,0 +1,9 @@
+<template>
+  <div id="search">
+    <h1>Search</h1>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
