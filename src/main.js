4c6bf88ca19ca6919f0f4b0b1bf695a2fb10fd86
mohamed17717 <root@localhost.localdomain>
2019-07-13 11:51:53 +0200

    install meta pkg to add meta tags, fonts etc...

--- src/main.js
+++ src/main.js
@@ -2,10 +2,12 @@ import Vue from "vue";
 import App from "@/App.vue";
 import store from "@/store";
 import MyPlugin from "@/plugins";
+import Meta from "vue-meta";
 
 Vue.config.productionTip = false;
 
 Vue.use(MyPlugin);
+Vue.use(Meta);
 
 new Vue({
   store,
