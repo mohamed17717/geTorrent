539aa512ba66f57b8acce145c27f80c008c253f8
mohamed17717 <root@localhost.localdomain>
2019-07-13 06:12:07 +0200

    import components in app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,19 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <Torrent />
+    <Subtitle />
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import Torrent from "./components/Torrent.vue";
+import Subtitle from "./components/Subtitle.vue";
 
 export default {
   name: "app",
   components: {
-    HelloWorld
+    Torrent,
+    Subtitle
   }
 };
 </script>
---
+++ src/components/Subtitle.vue
@@ -0,0 +1,11 @@
+<template>
+  <div id="subtitle">
+    <h1>Subtitle</h1>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
