3d1a0e2bf80743119fa4769a8caf0fc98638373e
mohamed17717 <root@localhost.localdomain>
2019-07-13 12:03:10 +0200

    add classes and style to search component

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,9 +1,31 @@
 <template>
-  <div id="search">
+  <div class="search search-container">
     <form v-on:submit.prevent="submitSearchForm">
-      <input type="text" v-model="film.name" />
-      <input type="number" v-model="film.year" min="1940" max="2019" />
-      <button type="submit">search</button>
+      <input
+        type="text"
+        id="movie-name"
+        name="movieName"
+        placeholder="اسم الفيلم"
+        autocomplete="off"
+        required
+        v-model="film.name"
+      />
+
+      <input
+        type="number"
+        id="movie-year"
+        name="movieYear"
+        min="1940"
+        max="2019"
+        placeholder="السنة"
+        required
+        v-model="film.year"
+      />
+
+      <button type="submit">
+        <span>بحث</span><i class="fa fa-search"></i>
+      </button>
+      <div id="google-link"><a target="_blank"></a></div>
     </form>
   </div>
 </template>
@@ -25,6 +47,7 @@ export default {
   methods: {
     submitSearchForm() {
       this.setFilmInfo(this.film);
+      // this.setProgressBar(5)
     },
 
     ...mapMutations(["setFilmInfo"])
