ccf88ef1418364400626f5bbd4a0d22083dc89fe
mohamed17717 <root@localhost.localdomain>
2019-07-13 06:34:07 +0200

    create search buttons

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,9 +1,21 @@
 <template>
   <div id="search">
-    <h1>Search</h1>
+    <input type="text" v-model="film.name" />
+    <input type="number" v-model="film.year" min="1940" max="2019" />
+    <button type="submit">search</button>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  name: "Search",
+  data() {
+    return {
+      film: {
+        name: "",
+        year: ""
+      }
+    };
+  }
+};
 </script>
