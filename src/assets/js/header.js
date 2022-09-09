250db03b5fbfa589ef211e3876d8fd3ce92f4cd0
d3v.mhmd <mohamad.mahmoud@florinz.com>
2022-09-09 09:33:45 +0200

    stop using ugly js and solve some issues in js

---
+++ src/assets/js/header.js
@@ -0,0 +1,61 @@
+// events
+
+function changeInputDirection(event) {
+  let firstLetter = event.target.value[0];
+
+  if (firstLetter && firstLetter.toUpperCase() != firstLetter.toLowerCase()) {
+    event.target.style.direction = "ltr";
+  } else {
+    event.target.style.direction = "rtl";
+  }
+}
+
+function shrinkText(text, num) {
+  let l = Math.floor((num - 3) / 2);
+  return text.slice(0, l) + "..." + text.slice(-l);
+}
+
+function getYearGoogleLink(event, outputSelector) {
+  let movieName = event.target.value;
+  let a = document.querySelector(outputSelector);
+  if (movieName) {
+    let link = `https://www.google.com/search?q=${movieName} movie year`;
+    let prefix = "ابحث عن السنة";
+    prefix += "<br>";
+    a.href = link;
+    a.innerHTML = prefix + shrinkText(link, 50);
+  } else {
+    a.innerText = "";
+  }
+}
+
+function handleHeader() {
+  const headerMovieName = document.querySelector("header #movie-name");
+  const header = document.querySelector("header div");
+
+  headerMovieName.addEventListener("keyup", function(e) {
+    changeInputDirection(e);
+    getYearGoogleLink(e, "#google-link a");
+  });
+
+  // header fixed
+  document.addEventListener("scroll", e => {
+    if (window.scrollY > header.clientHeight - 20) {
+      header.style.position = "fixed";
+      header.style.boxShadow = "rgb(158, 158, 158) 0px 5px 8px -4px";
+      header.style.width = "90%";
+      header.style.top = "0";
+      document.body.style.paddingTop = "60px";
+    } else {
+      header.style.position = "";
+      header.style.boxShadow = "";
+      header.style.width = "";
+      header.style.top = "";
+      document.body.style.paddingTop = "";
+    }
+  });
+}
+
+document.addEventListener("DOMContentLoaded", function(e) {
+  handleHeader();
+});
--- src/main.js
+++ src/main.js
@@ -5,7 +5,11 @@ import MyPlugin from "@/plugins";
 import Meta from "vue-meta";
 
 require("@/assets/css/style.css");
-require("@/assets/js/script.js");
+
+// require("@/assets/js/script.js");
+require("@/assets/js/block-steal.js");
+require("@/assets/js/header.js");
+require("@/assets/js/notes.js");
 
 Vue.config.productionTip = false;
 
